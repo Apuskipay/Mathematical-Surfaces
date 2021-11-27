b64d1fa2702bee59fef9279f83d45cfab560d8d0
Daniel Rondon <danielrondon00@gmail.com>
2021-11-27 00:00:06 -0400

    Created 3d functions
    The application is now capable of representing 3D graphs and functions.

--- Mathematical-Surfaces/Assets/Scripts/FunctionLibrary.cs
+++ Mathematical-Surfaces/Assets/Scripts/FunctionLibrary.cs
@@ -3,20 +3,30 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
-    public static float Wave(float x, float t)
+    public delegate float Function(float x, float z, float t);
+
+    public enum FunctionName { Wave, MultiWave, Ripple }
+
+    static Function[] functions = { Wave, MultiWave, Ripple };
+    public static Function GetFunction (FunctionName name)
     {
-        return Sin(PI * (x + t));
+        return functions[(int)name];
+    }
+    public static float Wave(float x, float z, float t)
+    {
+        return Sin(PI * (x + z + t));
     }
 
-    public static float MultiWave(float x, float t)
+    public static float MultiWave(float x, float z, float t)
     {
         float y = Sin(PI * (x + 0.5f * t));
-        y += 0.5f * Sin(2f * PI * (x + t));
-        return y * (2f / 3f);
+        y += 0.5f * Sin(2f * PI * (z + t));
+        y += Sin(PI * (x + z + 0.25f * t));
+        return y * (1f / 2.5f);
     }
-    public static float Ripple(float x, float t)
+    public static float Ripple(float x, float z, float t)
     {
-        float d = Abs(x);
+        float d = Sqrt(x * x + z * z);
         float y = Sin(PI * (4f * d - t));
         return y / (1f + 10f * d);
     }
--- Mathematical-Surfaces/Assets/Scripts/Graph.cs
+++ Mathematical-Surfaces/Assets/Scripts/Graph.cs
@@ -7,23 +7,29 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10,100)]
     int resolution = 10;
 
-    [SerializeField, Range(0, 2)]
-    int function;
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
 
     Transform[] points;
 
     // Awake is called when the script instance is being loaded
     void Awake()
     {
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++)
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
             float step = 2f / resolution;
             var position = Vector3.zero;
             var scale = Vector3.one * step;
             Transform point = points[i] = Instantiate(pointPrefab);
             point.localPosition = Vector3.right * i;
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
 
@@ -32,23 +38,13 @@ public class Graph : MonoBehaviour
     }
     private void Update()
     {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            if (function == 0)
-            {
-                position.y = FunctionLibrary.Wave(position.x, time);
-            }
-            else if (function == 1)
-            {
-                position.y = FunctionLibrary.MultiWave(position.x, time);
-            }
-            else if (function == 2)
-            {
-                position.y = FunctionLibrary.Ripple(position.x, time);
-            }
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
